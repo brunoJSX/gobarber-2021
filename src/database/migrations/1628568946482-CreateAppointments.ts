10eabfeacfe95a390049471ecb10a7a89aff2a54
Bruno Henrique <bruno@cbkdigital.com.br>
2021-08-10 01:07:58 -0400

    chore: changes

--- src/database/migrations/1628568946482-CreateAppointments.ts
+++ src/database/migrations/1628568946482-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1628568946482
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
